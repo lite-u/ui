fc4ad9fec807ccad9ceea1de2014d89560ef1eb1
yahone <yahone.chow@gmail.com>
2025-04-21 17:57:21 +0900

    modal

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import Input from './components/input/Input'
+import Button from './components/button/Button'
 import ThemeProvider from './themes/ThemeContext'
 import Modal from './components/modal/Modal'
 
-export {Input, ThemeProvider, Modal}
+export {Input, Button, ThemeProvider, Modal}
