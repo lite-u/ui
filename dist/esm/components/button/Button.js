a1da5242e8c9744451ddaeddd73fd6ca0170ed99
yahone <yahone.chow@gmail.com>
2025-04-21 18:41:57 +0900

    button

--- dist/esm/components/button/Button.js
+++ dist/esm/components/button/Button.js
@@ -1,5 +1,8 @@
 import { jsx as _jsx } from "react/jsx-runtime";
+import { useTheme } from '../../themes/ThemeContext';
 const Button = ({ type, ...props }) => {
+    const theme = useTheme();
+    console.log(theme);
     return _jsx("button", { type: type, ...props });
 };
 export default Button;
--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,8 +1,11 @@
+import {useTheme} from '../../themes/ThemeContext'
+
 const Button: React.FC<React.ButtonHTMLAttributes<HTMLButtonElement> & { size: 'sm' | 'md' | 'xl' }> = ({
                                                                                                           type,
                                                                                                           ...props
                                                                                                         }) => {
-
+  const theme = useTheme()
+  console.log(theme)
   return <button type={type} {...props}/>
 }
 
