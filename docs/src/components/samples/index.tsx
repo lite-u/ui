9bb027182b0397a9f60b0347c78ff9a2a9f3bde7
yahone <yahone.chow@gmail.com>
2025-04-24 18:04:06 +0900

    ###

---
+++ docs/src/components/samples/index.tsx
@@ -0,0 +1,68 @@
+import ButtonsSample from './ButtonsSample.tsx'
+import InputsSample from './InputsSample.tsx'
+import SelectSamples from './SelectSamples.tsx'
+import PanelSamples from './PanelSamples.tsx'
+import LinkSamples from './LinkSamples.tsx'
+import MenuItemSamples from './MenuItemSamples.tsx'
+import MenuSamples from './MenuSamples.tsx'
+import ModalSamples from './ModalSamples.tsx'
+import RowSamples from './RowSamples.tsx'
+import TitleSamples from './TitleSamples.tsx'
+import ConSamples from './ConSamples.tsx'
+
+const SAMPLES = [
+  {
+    name: 'Buttons',
+    comp: <ButtonsSample/>,
+  },
+  {
+    name: 'Inputs',
+    comp: <InputsSample/>,
+  },
+  {
+    name: 'Select',
+    comp: <SelectSamples/>,
+  },
+  {
+    name: 'Link',
+    comp: <LinkSamples/>,
+  },
+  {
+    name: 'Modal',
+    comp: <ModalSamples/>,
+  },
+  {
+    name: 'Panel',
+    comp: <PanelSamples/>,
+  },
+  {
+    name: 'Con',
+    comp: <ConSamples/>,
+  },
+  {
+    name: 'Row',
+    comp: <RowSamples/>,
+  },
+  {
+    name: 'Col',
+    comp: <ConSamples/>,
+  },
+  {
+    name: 'Title',
+    comp: <TitleSamples/>,
+  },
+  {
+    name: 'Paragraph',
+    comp: <PanelSamples/>,
+  },
+  {
+    name: 'Menu',
+    comp: <MenuSamples/>,
+  },
+  {
+    name: 'MenuItem',
+    comp: <MenuItemSamples/>,
+  },
+]
+
+export default SAMPLES
