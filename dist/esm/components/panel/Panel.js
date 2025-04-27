6f0753f99022a72c6df4f0e68f15cde40d480065
yahone <yahone.chow@gmail.com>
2025-04-27 21:54:05 +0900

    ###

--- dist/esm/components/panel/Panel.js
+++ dist/esm/components/panel/Panel.js
@@ -3,12 +3,14 @@ import { useLiteUIContext } from '../../LiteUIProvider';
 import { Col } from '../../index';
 import Paragraph from '../paragraph/Paragraph';
 import { Con } from '../con/Con';
-const Panel = ({ title, titleClass = '', boxClass = '', children, ...props }) => {
+const Panel = ({ title, titleClass = '', titleStyle = {}, boxClass = '', boxStyle = {}, children, ...props }) => {
     const { theme } = useLiteUIContext();
-    return _jsx(Con, { fw: true, fh: true, role: 'panel', ...props, children: _jsxs(Col, { fw: true, fh: true, stretch: true, children: [_jsx(Con, { fw: true, style: { background: theme.panel.primaryColor, borderRadius: '3px 3px 0 0', color: '#fff' }, className: titleClass, children: _jsx(Paragraph, { center: true, size: 16, style: { padding: 8 }, children: title }) }), _jsx(Con, { fw: true, bg: '#fff', style: {
+    return _jsx(Con, { fw: true, fh: true, role: 'panel', ...props, children: _jsxs(Col, { fw: true, fh: true, stretch: true, children: [_jsx(Con, { role: 'panel-head', fw: true, style: { background: theme.panel.primaryColor, borderRadius: '3px 3px 0 0', color: '#fff', ...titleStyle }, className: titleClass, children: _jsx(Paragraph, { center: true, size: 16, style: { padding: 8 }, children: title }) }), _jsx(Con, { role: 'panel-box', fw: true, bg: '#fff', style: {
+                        overflow: 'auto',
                         border: `1px solid ${theme.panel.primaryColor}`,
                         borderTop: 'none',
                         flex: '100%',
+                        ...boxStyle,
                     }, className: boxClass, children: children })] }) });
 };
 export default Panel;
--- dist/types/components/panel/Panel.d.ts
+++ dist/types/components/panel/Panel.d.ts
@@ -2,7 +2,9 @@ import { ReactNode } from 'react';
 import { ContainerProps } from '../con/Con';
 declare const Panel: React.FC<{
     title: string;
+    titleStyle?: React.CSSProperties;
     titleClass?: string;
+    boxStyle?: React.CSSProperties;
     boxClass?: string;
     children?: ReactNode;
 } & ContainerProps & React.HTMLProps<HTMLDivElement>>;
