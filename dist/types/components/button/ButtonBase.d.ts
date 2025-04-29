8df036c968c4fd2b53057d2b7c1e4f886e77874f
yahone <yahone.chow@gmail.com>
2025-04-29 10:54:00 +0900

    transition

---
+++ dist/types/components/button/ButtonBase.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="react" />
+export declare const ButtonBase: React.FC<React.ButtonHTMLAttributes<HTMLButtonElement> & {
+    style?: React.CSSProperties;
+}>;
+export default ButtonBase;
