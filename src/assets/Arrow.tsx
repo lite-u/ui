14d4257939afd46fb51335ec6c74c8629888d5b9
yahone <yahone.chow@gmail.com>
2025-04-30 20:48:40 +0900

    ###

---
+++ src/assets/Arrow.tsx
@@ -0,0 +1,12 @@
+export const IconArrowDown = () => {
+  return <svg width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"
+              strokeLinecap="round" strokeLinejoin="round">
+    <polyline points="18 15 12 9 6 15"/>
+  </svg>
+}
+export const IconArrowUp = () => {
+  return <svg width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2"
+              strokeLinecap="round" strokeLinejoin="round">
+    <polyline points="6 9 12 15 18 9"/>
+  </svg>
+}
