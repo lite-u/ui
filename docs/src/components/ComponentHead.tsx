64ccc550caa3fbafa18175dd9d25898b1420005e
yahone <yahone.chow@gmail.com>
2025-04-30 23:55:10 +0900

    ###

--- docs/src/components/ComponentHead.tsx
+++ docs/src/components/ComponentHead.tsx
@@ -1,22 +1,20 @@
 import {Col, Con, Title} from '@lite-u/ui'
 import CodeWrap from './codeBlock/CodeWrap.tsx'
-import {useLocation} from 'react-router'
+import {Link, useLocation} from 'react-router'
 import COMPONENT_ROUTE_MAP from '../pages/componentData.tsx'
 
 const ComponentHead = () => {
   const location = useLocation()
-  // console.log(location.pathname);
   const currentPath = location.pathname.split('/')[2]
-  console.log(currentPath)
+
   const COMP_ROUTE = COMPONENT_ROUTE_MAP[currentPath as keyof typeof COMPONENT_ROUTE_MAP]
 
   if (!COMP_ROUTE) return
 
   return <Con>
-    <Col id={COMP_ROUTE.to}>
-      12
+    <Col>
+      <Title id={COMP_ROUTE.to} sticky><Link to={COMP_ROUTE.to}>#</Link> {COMP_ROUTE.name}</Title>
       <Con m={20}></Con>
-      <Title h2>{COMP_ROUTE.name}</Title>
       <Title h4>Import</Title>
       <CodeWrap code={`import { ${COMP_ROUTE.name} } from '@lite-u/ui'`}/>
       <Con m={'0 0 20px 0'}></Con>
--- docs/src/pages/layouts/ComponentLayout.tsx
+++ docs/src/pages/layouts/ComponentLayout.tsx
@@ -2,14 +2,9 @@ import {Outlet} from 'react-router'
 import ComponentHead from '../../components/ComponentHead.tsx'
 
 const ComponentLayout = () => {
-  /*const location = useLocation();
-  // console.log(location.pathname);
-  const currentPath = location.pathname.split('/')[2]*/
-  // console.log(currentPath)
   return <div>
     <ComponentHead/>
     <Outlet/>
-
   </div>
 }
 
