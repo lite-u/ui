18d3016bd6584d93a5df3cfb3aa249cd59a45e54
yahone <yahone.chow@gmail.com>
2025-05-01 17:04:49 +0900

    1.0.0

--- docs/src/components/codeBlock/MDBlock.tsx
+++ docs/src/components/codeBlock/MDBlock.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Markdown from 'react-markdown'
 
-const MDBlock: React.FC<React.HTMLProps<HTMLDivElement> & { children:string }> = ({children, ...props}) => {
+const MDBlock: React.FC<React.HTMLProps<HTMLDivElement> & { children: string }> = ({children, ...props}) => {
   return <div {...props}><Markdown>{children}</Markdown></div>
 }
 
