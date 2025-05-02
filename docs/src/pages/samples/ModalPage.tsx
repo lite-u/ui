cb50cda3412237492f52d5ad3992cae935e6f55c
yahone <yahone.chow@gmail.com>
2025-05-02 23:18:25 +0900

    1.0.0

--- docs/src/pages/samples/ModalPage.tsx
+++ docs/src/pages/samples/ModalPage.tsx
@@ -8,7 +8,33 @@ const ModalPage: React.FC = () => {
     <Row end space={10}>
       <Con w={300}>
         <UsageBlock title={'Link'} replacement={`
+          import {Button, Col, Con, Modal, Row} from '@lite-u/ui'
+
+          const ModalSample: React.FC = () => {
+            const [showModal, setShowModal] = useState(false)
           
+            return <>
+              <Button onClick={() => {
+                setShowModal(true)
+              }}>Open Modal</Button>
+          
+              {
+                showModal && <Modal onBackdropClick={() => {
+                  setShowModal(false)
+                }}>
+                      <Con w={'90%'} h={'90%'} bg={'white'}>
+                          <Row fw fh center jc>
+                              <Col center>
+                                  <Button onClick={() => {
+                                    setShowModal(false)
+                                  }}>Close Modal</Button>
+                              </Col>
+                          </Row>
+                      </Con>
+                  </Modal>
+              }
+            </>
+          }
         `}>
           <ModalSample/>
         </UsageBlock>
