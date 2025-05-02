ae3891e20cc682d4f49710dfaca11f24cdfe6c08
yahone <yahone.chow@gmail.com>
2025-05-02 22:59:24 +0900

    1.0.0

--- docs/src/pages/samples/ModalPage.tsx
+++ docs/src/pages/samples/ModalPage.tsx
@@ -1,53 +1,46 @@
 import {Button, Col, Con, Modal, Row} from '@lite-u/ui'
 import {useState} from 'react'
-import CodeWrap from '../../components/codeBlock/CodeWrap.tsx'
+import UsageBlock from '../UsageBlock.tsx'
 
 const ModalPage: React.FC = () => {
-  const [showModal, setShowModal] = useState(false)
+  // const [showModal, setShowModal] = useState(false)
   return <Col>
-
-    <Col between style={{}}>
-      <Con>
-        <Button onClick={() => {
-          setShowModal(true)
-        }}>Open Modal</Button>
+    <Row end space={10}>
+      <Con w={300}>
+        <UsageBlock title={'Link'} replacement={`
+          
+        `}>
+          <ModalSample/>
+        </UsageBlock>
       </Con>
-      <Con m={10}></Con>
-
-      {
-        showModal && <Modal onBackdropClick={() => {
-          setShowModal(false)
-        }}>
-              <Con w={'90%'} h={'90%'} bg={'white'}>
-                  <Row fw fh center jc>
-                      <Col center>
-                          <Button onClick={() => {
-                            setShowModal(false)
-                          }}>Close Modal</Button>
-                      </Col>
-                  </Row>
-              </Con>
-          </Modal>
-      }
-
-      <CodeWrap code={`
-      <Modal onBackdropClick={() => {
-          setShowModal(false)
-        }}>
-          <Con w={'90%'} h={'90%'} bg={'white'}>
-              <Row fw fh center jc>
-                  <Col center>
-                      <Button onClick={() => {
-                        setShowModal(false)
-                      }}>Close Modal</Button>
-                  </Col>
-              </Row>
-          </Con>
-      </Modal>
-      `}></CodeWrap>
-
-    </Col>
+    </Row>
   </Col>
+}
+
+const ModalSample: React.FC = () => {
+  const [showModal, setShowModal] = useState(false)
+
+  return <>
+    <Button onClick={() => {
+      setShowModal(true)
+    }}>Open Modal</Button>
+
+    {
+      showModal && <Modal onBackdropClick={() => {
+        setShowModal(false)
+      }}>
+            <Con w={'90%'} h={'90%'} bg={'white'}>
+                <Row fw fh center jc>
+                    <Col center>
+                        <Button onClick={() => {
+                          setShowModal(false)
+                        }}>Close Modal</Button>
+                    </Col>
+                </Row>
+            </Con>
+        </Modal>
+    }
+  </>
 }
 
 export default ModalPage
