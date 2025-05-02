be15566ad338e5ec688af5e770e6c37f06cfee0d
yahone <yahone.chow@gmail.com>
2025-05-02 16:10:31 +0900

    1.0.0

--- docs/src/pages/samples/InputPage.tsx
+++ docs/src/pages/samples/InputPage.tsx
@@ -1,4 +1,4 @@
-import {Col, Con, Input, P, Row, Title} from '@lite-u/ui'
+import {Col, Con, Input, Row, Title} from '@lite-u/ui'
 import {useEffect, useRef} from 'react'
 import UsageBlock from '../UsageBlock.tsx'
 
@@ -12,62 +12,70 @@ const InputPage: React.FC = () => {
     <Title h4>Primary</Title>
 
 
-    <Row center between>
-      <Con w={200}>
+    <Row start style space={10} wrap>
+      <Con w={220}>
         <UsageBlock title={'xs'}>
+          <Col between start space={10}>
+            <Input label={'Name1:'} placeholder={'xs size warn'} xs/>
+            <Input label={'Name2:'} placeholder={'xs size warn'} warn xs/>
+            <Input label={'Name3:'} placeholder={'xs size error'} error xs/>
+            <Input label={'First Name:'} placeholder={'xs size default'} xs/>
+          </Col>
+        </UsageBlock>
+      </Con>
+
+      <Con w={200}>
+        <UsageBlock title={'Small'}>
           <Col between center space={10}>
-            <Input placeholder={'xs size default'} xs/>
-            <Input placeholder={'xs size warn'} warn xs/>
-            <Input placeholder={'xs size error'} error xs/>
+            <Input placeholder={'s size default'} s/>
+            <Input placeholder={'s size warn'} warn s/>
+            <Input placeholder={'s size error'} error s/>
+          </Col>
+        </UsageBlock>
+      </Con>
+
+      <Con w={220}>
+        <UsageBlock title={'Medium'}>
+          <Col between center space={10}>
+            <Input placeholder={'m size default'}/>
+            <Input placeholder={'m size warn'} warn/>
+            <Input placeholder={'m size error'} error/>
+          </Col>
+        </UsageBlock>
+      </Con>
+
+      <Con w={300}>
+        <UsageBlock title={'Large'}>
+          <Col between center space={10}>
+            <Input placeholder={'l size default'} l/>
+            <Input placeholder={'l size warn'} warn l/>
+            <Input placeholder={'l size error'} error l/>
+          </Col>
+        </UsageBlock>
+      </Con>
+
+      <Con w={300}>
+        <UsageBlock title={'Number'}>
+          <Col between center space={10}>
+            <Input number value={1} step={2} placeholder={'size default'}/>
+            <Input number value={1} step={2} placeholder={'size warn'} warn/>
+            <Input number value={1} step={2} placeholder={'size error'} error/>
           </Col>
         </UsageBlock>
       </Con>
 
     </Row>
+    {/*
+    <Con w={300}>
+      <UsageBlock title={'Large'}>
+        <Col between center space={10}>
+          <Input placeholder={'l size default'} l/>
+          <Input placeholder={'l size warn'} warn l/>
+          <Input placeholder={'l size error'} error l/>
+        </Col>
+      </UsageBlock>
+    </Con>*/}
 
-    <Con w={600}>
-      <P>xs</P>
-      <Row space={6}>
-        <Input xs/>
-        <Input warn xs/>
-        <Input error xs/>
-      </Row>
-
-      <P>Small</P>
-      <Row space={6}>
-        <Input s/>
-        <Input warn s/>
-        <Input error s/>
-      </Row>
-
-      <P>Medium</P>
-
-      <Row space={6}>
-        <Input m/>
-        <Input warn m/>
-        <Input error m/>
-      </Row>
-
-      <P>Large</P>
-
-      <Row space={6}>
-        <Input l/>
-        <Input warn l/>
-        <Input error l/>
-      </Row>
-    </Con>
-
-    <Con>
-      <P>Number</P>
-
-      <Row space={6}>
-        <Input step={0.1} placeholder={'hello'} number/>
-        <Input step={0.1} number warn/>
-        <Input number error/>
-      </Row>
-    </Con>
-
-    <Input label={'hello'}/>
   </Col>
 }
 
