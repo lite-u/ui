240f36f7bdf07b2373961763806444b3cf957669
yahone <yahone.chow@gmail.com>
2025-05-02 13:01:36 +0900

    1.0.0

--- docs/src/components/codeBlock/codeBlock.tsx
+++ docs/src/components/codeBlock/codeBlock.tsx
@@ -1,27 +1,14 @@
-import React, {CSSProperties, useEffect, useRef} from 'react'
-import hljs from 'highlight.js/lib/core'
-import typescript from 'highlight.js/lib/languages/typescript'
-// import 'highlight.js/styles/github.css'
-// import 'highlight.js/styles/atom-one-light.min.css'
+import React from 'react'
+// import typescript from 'highlight.js/lib/languages/typescript'
 import SyntaxHighlighter from 'react-syntax-highlighter'
 import {githubGist} from 'react-syntax-highlighter/dist/esm/styles/hljs'
 
-hljs.registerLanguage('typescript', typescript)
+// hljs.registerLanguage('typescript', typescript)
 
 const CodeBlock: React.FC<React.HTMLProps<HTMLDivElement> & {
   code: string
-  language?: 'language-typescript'
-  codeStyle?: CSSProperties
-}> = ({code, language = 'language-typescript'}) => {
-  const ref = useRef<HTMLElement>(null)
-
-  useEffect(() => {
-    if (ref.current) {
-      // delete ref.current.dataset.highlighted
-      // hljs.highlightElement(ref.current)
-    }
-  }, [code, language, ref.current])
-  console.log(githubGist)
+  // codeStyle?: CSSProperties
+}> = ({code}) => {
   return <SyntaxHighlighter language="javascript" style={{
     ...githubGist,
     hljs: {...githubGist.hljs, backgroundColor: 'transparent'},
