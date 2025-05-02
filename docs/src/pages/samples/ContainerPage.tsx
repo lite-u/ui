103abf07fc8e291889429007707baf552f1e780a
yahone <yahone.chow@gmail.com>
2025-05-02 23:44:04 +0900

    1.0.0

--- docs/src/pages/samples/ContainerPage.tsx
+++ docs/src/pages/samples/ContainerPage.tsx
@@ -1,41 +1,22 @@
-import {Col, Con, Panel, Row,  } from '@lite-u/ui'
-import CodeWrap from '../../components/codeBlock/CodeWrap.tsx'
+import {Col, Con, Row} from '@lite-u/ui'
+import UsageBlock from '../UsageBlock.tsx'
 
 const ContainerPage: React.FC = () => {
-  return <Col  >
-
-    <Col between>
-
-      <Con w={200} h={200} p={20} bg={'blue'}>
-        <Con fh p={20} bg={'green'}>
-          <Con fh p={20} bg={'red'}>
-            <Con fh p={20} bg={'yellow'} box={false}></Con>
+  return <Col>
+    <Row space={10} wrap>
+      <Con w={300}>
+        <UsageBlock title={'xs'}>
+          <Con w={200} h={200} p={20} bg={'blue'}>
+            <Con fh p={20} bg={'green'}>
+              <Con fh p={20} bg={'red'}>
+                <Con fh p={20} bg={'yellow'} box={false}></Con>
+              </Con>
+            </Con>
           </Con>
-        </Con>
+        </UsageBlock>
       </Con>
 
-      <Con m={20}></Con>
-
-      <CodeWrap code={`
-      <Con w={200} h={200} p={20} bg={'blue'}>
-        <Con fh p={20} bg={'green'}>
-          <Con fh p={20} bg={'red'}>
-            <Con fh p={20} bg={'yellow'} box={false}></Con>
-          </Con>
-        </Con>
-      </Con>
-        `}/>
-
-      <Con m={20}></Con>
-
-      <Col>
-        <Row>
-          <Col w={400}>
-            <Panel title={'Con API'}></Panel>
-          </Col>
-        </Row>
-      </Col>
-    </Col>
+    </Row>
   </Col>
 }
 
