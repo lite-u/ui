9e5a00ffdb6fa9f9afe93d6457ad9bdd926d9f17
yahone <yahone.chow@gmail.com>
2025-05-02 20:44:21 +0900

    1.0.0

---
+++ dist/types/components/AABBCC.d.ts
@@ -0,0 +1,8 @@
+declare const Abc: React.FC<React.HTMLProps<HTMLDivElement> & {
+    /**
+     *  size variant for the select.
+     * @default false
+     */
+    xs?: boolean;
+}>;
+export default Abc;
