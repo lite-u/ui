c086f50c9e3f645fa4dedb5007d9a13427f44444
yahone <yahone.chow@gmail.com>
2025-05-04 18:33:00 +0900

    1.0.0

--- docs/src/pages/samples/TableRowPage.tsx
+++ docs/src/pages/samples/TableRowPage.tsx
@@ -1,29 +1,27 @@
-import {Col, Con, Flex, Row, Table, TableRow} from '@lite-u/ui'
+import {Col, Con, Table, TableRow} from '@lite-u/ui'
 import SampleBlock from '../SampleBlock.tsx'
 import {NavLink} from 'react-router'
 
 const TableRowPage: React.FC = () => {
   return <Col id={'col'}>
-    <Row space={10} mt={10} start wrap>
-      <Flex col minH={260} w={200}>
-        <SampleBlock title={'XS Size'} style={{flex: 1}}>
-          <Table xs>
-            <TableRow head>
-              <h1>name</h1>
-              <h2>age</h2>
-            </TableRow>
-            <TableRow>
-              <div>Puma</div>
-              <p>1970</p>
-            </TableRow>
-            <TableRow>
-              <span>Nike</span>
-              <span>1970</span>
-            </TableRow>
-          </Table>
-        </SampleBlock>
-      </Flex>
-    </Row>
+    <Col w={200}>
+      <SampleBlock title={'XS Size'} style={{flex: 1}}>
+        <Table xs>
+          <TableRow head>
+            <h1>name</h1>
+            <h2>age</h2>
+          </TableRow>
+          <TableRow>
+            <div>Puma</div>
+            <p>1970</p>
+          </TableRow>
+          <TableRow>
+            <span>Nike</span>
+            <span>1970</span>
+          </TableRow>
+        </Table>
+      </SampleBlock>
+    </Col>
 
     <Con mt={50} fz={16}>
       See also <NavLink to={'/components/table'}>Table</NavLink>
