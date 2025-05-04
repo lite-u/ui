2490c3e38b0607d6a2a2fe39ae4108e0a318c107
yahone <yahone.chow@gmail.com>
2025-05-04 09:27:34 +0900

    1.0.0

--- docs/src/pages/samples/ColumnPage.tsx
+++ docs/src/pages/samples/ColumnPage.tsx
@@ -1,18 +1,6 @@
 import {Button, Col, Con, Row} from '@lite-u/ui'
 import UsageBlock from '../UsageBlock.tsx'
 
-const HeartIcon = () => {
-  return <svg viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg" width="24" height="24">
-    <path d="M12 21.35c-.45-.42-1.14-1.01-1.94-1.72C5.92 15.3 2 12.05 2 8.28
-           2 5.5 4.5 3 7.28 3
-           c1.91 0 3.63 1.06 4.72 2.72
-           C13.09 4.06 14.81 3 16.72 3
-           19.5 3 22 5.5 22 8.28
-           c0 3.77-3.92 7.02-8.06 11.35
-           -.8.71-1.49 1.3-1.94 1.72z"/>
-  </svg>
-}
-
 const ColumnPage: React.FC = () => {
   return <Col>
     <Row start space={10} wrap>
