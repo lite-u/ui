4544f4e5d10c1f49163e4e33b625a672bd9fa64c
yahone <yahone.chow@gmail.com>
2025-05-04 12:54:59 +0900

    1.0.0

--- docs/src/pages/samples/NotificationPage.tsx
+++ docs/src/pages/samples/NotificationPage.tsx
@@ -7,6 +7,12 @@ const NotificationPage: React.FC = () => {
   return <Col>
     <Row start space={10} wrap>
 
+      <Con w={200}>
+        <UsageBlock title={'Show a new Notification'} replacement={``}>
+          <NotificationSampleSimple/>
+        </UsageBlock>
+      </Con>
+
       <Con w={'auto'}>
         <UsageBlock title={'Toggle a Notification'} replacement={`
         const NotificationSample: React.FC = () => {
@@ -37,7 +43,7 @@ const NotificationPage: React.FC = () => {
         }
 
         `}>
-          <NotificationSample/>
+          <NotificationSampleToggle/>
         </UsageBlock>
       </Con>
 
@@ -45,14 +51,48 @@ const NotificationPage: React.FC = () => {
   </Col>
 }
 
-const NotificationSample: React.FC = () => {
+const NotificationSampleSimple: React.FC = () => {
+  const {add} = useNotification()
+
+  return <Col between>
+    <Button onClick={() => { add('Hello: ' + new Date().toLocaleString('en-US')) }}>Show</Button>
+  </Col>
+}
+
+const NotificationSampleToggle: React.FC = () => {
   const {add, remove} = useNotification()
   const notificationId = useRef<string>(undefined)
 
   return <Col between>
     {
       !notificationId.current ?
-        <Button onClick={() => {
+        <Button primary onClick={() => {
+          if (notificationId.current) {
+            remove(notificationId.current)
+          }
+          notificationId.current = add(
+            'A notification that stays centered on the screen until it is manually removed.',
+            'info',
+            false,
+          )
+        }}>Show</Button>
+        :
+        <Button warn onClick={() => {
+          remove(notificationId.current as string)
+          notificationId.current = undefined
+        }}>Hide</Button>
+    }
+
+  </Col>
+}
+const NotificationSampleComp: React.FC = () => {
+  const {add, remove} = useNotification()
+  const notificationId = useRef<string>(undefined)
+
+  return <Col between>
+    {
+      !notificationId.current ?
+        <Button primary onClick={() => {
           if (notificationId.current) {
             remove(notificationId.current)
           }
