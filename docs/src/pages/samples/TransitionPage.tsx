647654dc21e514ef208898f25f2251c2c0bbf62b
yahone <yahone.chow@gmail.com>
2025-05-04 19:27:45 +0900

    1.0.0

--- docs/src/pages/samples/TransitionPage.tsx
+++ docs/src/pages/samples/TransitionPage.tsx
@@ -175,9 +175,9 @@ const TransitionDelayed = () => {
       }}
       delay={1000}
       visible={visible}
-      style={{width: 100, height: 30}}
+      style={{width: 100, height: 30,backgroundColor:'#146e18',color:'#fff'}}
       from={{width: 100}}
-      to={{width: 200}}>
+      to={{width: 120}}>
       Hover On And Wait
     </Transition>
 
