30777e3252d943765f550727af0534ff44e05c73
yahone <yahone.chow@gmail.com>
2025-05-07 17:43:50 +0900

    1.0.0

--- src/LiteUIProvider.tsx
+++ src/LiteUIProvider.tsx
@@ -1,15 +1,7 @@
 import {createContext, useContext, useEffect, useState} from 'react'
 import {liteTheme} from './themes/lite.theme'
-// import {darkTheme} from './themes/dark'
-// import {ThemeType} from './themes/type'
-import NotificationProvider from './components/notification/NotificationProvider'
 import {ThemeType} from './themes/type'
-/* eslint disable */
 
-/**
- * List of LiteUIContext
- * @default []
- */
 export const LiteUIContext = createContext<{
   theme: ThemeType
   setTheme: (mode: 'lite') => void
@@ -34,9 +26,7 @@ const LiteUIProvider = ({children}: { children: React.ReactNode }) => {
 
   return (
     <LiteUIContext.Provider value={{theme: currentTheme, setTheme}}>
-      <NotificationProvider>
-        {children}
-      </NotificationProvider>
+      {children}
     </LiteUIContext.Provider>
   )
 }
